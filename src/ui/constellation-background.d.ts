2756bff96c832251c2bb0f11dbac879116b389bb
João Carreira <joao@pixelapoteose.com>
2025-11-12 16:45:05 +0000

    fix: add manual type definition for ConstellationBackground

---
+++ src/ui/constellation-background.d.ts
@@ -0,0 +1,6 @@
+interface ConstellationBackgroundProps {
+  count?: number;
+  className?: string;
+}
+
+export function ConstellationBackground({ count, className }: ConstellationBackgroundProps): JSX.Element;
